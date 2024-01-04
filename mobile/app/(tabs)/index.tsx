0ff1d79673b61e6fabd4d500552fa639bab554f4
MateusM3 <letsgood56@gmail.com>
2024-01-04 09:00:31 -0300

    style: FASTFOOD-003 added custom font

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -6,6 +6,15 @@ import { Text, View } from '../../components/Themed';
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
+      <View
+        style={styles.averageCost}
+      >
+        <Text
+          style={styles.averageCostLabel}
+        >
+          $1.90
+        </Text>
+      </View>
       <Text style={styles.title}>Tab 1</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
@@ -28,4 +37,21 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  averageCost: {
+    flexDirection: 'column-reverse',
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignSelf: 'center',
+    backgroundColor: '#FF3D71',
+    marginTop: 6,
+    paddingVertical: 3,
+    paddingHorizontal: 7,
+    borderRadius: 10
+  },
+  averageCostLabel: {
+    fontFamily: 'Inter',
+    fontWeight: '400',
+    fontSize: 12,
+    color: '#FFFFFF'
+  }
 });
